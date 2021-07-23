1abdeff83ac3c5276cd57f87a11f745796bf325c
Genyus <genyus@gmail.com>
2021-07-23 13:36:24 +0100

    refactor: parse output.txt only once

--- .autograding/__tests__/test1.js
+++ .autograding/__tests__/test1.js
@@ -40,65 +40,54 @@ describe('file size', () => {
 
 // command tests
 describe('commands', () => {
-  test('created folder1', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('mkdir folder1');
+  let commands = [];
+
+  beforeAll(done => {
+    getCommands('../output.txt', (data) => {
+      commands = data;
+      done();
     });
+  });
+
+  test('created folder1', () => {
+    expect(commands).toContain('mkdir folder1');
   });
 
   test('created folder2', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('mkdir folder2');
-    });
+    expect(commands).toContain('mkdir folder2');
   });
 
   test('created file1.txt', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('touch file1.txt');
-    });
+    expect(commands).toContain('touch file1.txt');
   });
   
   test('created file2.txt', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('touch file2.txt');
-    });
+    expect(commands).toContain('touch file2.txt');
   });
   
   test('created file3.txt', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('touch file3.txt');
-    });
+    expect(commands).toContain('touch file3.txt');
   });
   
   test('moved file1.txt', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('mv file1.txt folder1');
-    });
+    expect(commands).toContain('mv file1.txt folder1');
   });
   
   test('moved file2.txt', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('mv file2.txt folder2');
-    });
+    expect(commands).toContain('mv file2.txt folder2');
   });
   
   test('deleted file3.txt', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('rm file3.txt');
-    });
+    expect(commands).toContain('rm file3.txt');
   });
   
   test('listed folder1 contents', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('cd folder1');
-      expect(commands.find(command => command.startsWith('ls'))).toBeTruthy();
-    });
+    expect(commands).toContain('cd folder1');
+    expect(commands.find(command => command.startsWith('ls'))).toBeTruthy();
   });
   
   test('created full.txt', () => {
-    getCommands('../output.txt', (commands) => {
-      expect(commands).toContain('dd if=/dev/zero bs=1 count=0 seek=2m of=full.txt');
-    });
+    expect(commands).toContain('dd if=/dev/zero bs=1 count=0 seek=2m of=full.txt');
   });
 
 });
